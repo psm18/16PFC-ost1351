22ab49cb0d18153ea9af548f2f6f5e88fa673ede
CAD Client <CAD Client>
2016-03-29 10:53:47 +0900

    added often

---
+++ ex05/ex05.py
@@ -0,0 +1,17 @@
+my_name = 'Park sun myung'
+my_age = 35
+my_height_cm = 160.1
+my_weight_kg = 55
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." %my_name
+print "She's %g inches tall." %my_height_cm
+print "She's %d pounds heavy." %my_weight_kg
+print "Actually that's not too heavy"
+print "She's got %s eyes and %s hair." %(my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." %my_teeth
+
+
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height_cm, my_weight_kg, my_age+my_height_cm+my_weight_kg)
