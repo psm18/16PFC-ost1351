f05a8f571618c9053ea8c8315db495934d38ba9e
CAD Client <CAD Client>
2016-05-14 11:20:24 +0900

    ex28

---
+++ ex28/ex28.py
@@ -0,0 +1,10 @@
+
+
+
+print("1.True and True=%s" %str(True and True))
+print("2.False and true=%s" %str(False and True))
+print("3.1==1 and 2==1 =%s" %((1==1)and(2==1)))
+print("4. 'test'=='test' =%s" %((1==1)or(2 != 1)))
+print("5. 1==1 or 2!=1 = %s" %((1==1)or(2 != 1)))
+print("6. True and 1==1 =%s" %str(True and (1==1)))
+print("7. False and 0!=0 = %s" %str(True and(0)))
